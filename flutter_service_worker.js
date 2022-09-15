240dca7f73b16aa80c0f8bac19235b472187ff16
Mehdi <mehdi_boulhalfa@live.fr>
2022-09-15 22:03:26 +0200

    Deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2eee636098cfa25c314d22207e4720fa",
-"/": "2eee636098cfa25c314d22207e4720fa",
-"main.dart.js": "e0eaf4602a7027380b98908a2ca69fbc",
+"index.html": "fb761270551a81255fc894e9265b6fda",
+"/": "fb761270551a81255fc894e9265b6fda",
+"main.dart.js": "736f6cc6aa84d5e431cf45d50e29a311",
 "manifest.json": "8987c3a193696841b2da85b908379d40",
 "version.json": "959abfb229cb3beb307027f987bcc827"
 };
